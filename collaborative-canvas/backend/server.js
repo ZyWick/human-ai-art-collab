0cadc09162843f75432058ea3b56620c2b79d042
ramosronn <ronn_ramos@dlsu.edu.ph>
2025-03-09 15:38:12 +0800

    Update server.js
    Error: No 'Access-Control-Allow-Origin' Fix

--- collaborative-canvas/backend/server.js
+++ collaborative-canvas/backend/server.js
@@ -31,6 +31,16 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "React app URL"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 app.use(passport.initialize());

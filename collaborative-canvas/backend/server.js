8221f67676211a9bb5f6234de4a234624647e273
ramosronn <ronn_ramos@dlsu.edu.ph>
2025-03-09 22:05:05 +0800

    Update server.js
    Acess-Control-Allow-Origin

--- collaborative-canvas/backend/server.js
+++ collaborative-canvas/backend/server.js
@@ -45,6 +45,16 @@ app.use(cors({
 }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "React app URL"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 // 🔹 Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))

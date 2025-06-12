1cbe613f3f8f4348978b7d12fe1647ec046b340c
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2025-06-12 13:49:17 +0800

    eslint

--- collaborative-canvas/frontend/src/components/layout/DesignWorkspace.jsx
+++ collaborative-canvas/frontend/src/components/layout/DesignWorkspace.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Konva from "konva";
 import DesignDetails from "../widgets/DesignDetails";
 import UploadButton from "../widgets/UploadButton";
